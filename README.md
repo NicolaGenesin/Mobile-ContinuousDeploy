105033b1bc5525df6e701038e0738507fc767b99
Nicola Genesin <nicolagenesin@yahoo.it>
2015-08-09 04:56:02 +0100

    first commit, some bp needs to be removed

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Mobile-ContinuousDeploy
+#### Mobile-ContinuousDeploy
+
+![](http://s18.postimg.org/tf46ty0qh/Screen_Shot_2015_08_09_at_04_42_39.png)
+
+Simple node app to watch your artifacts during CI(Jenkins, Teamcity, etc.).
+
+Should be used as postbuild action in order to deploy your apk (android) or zip (ios) on Appetize.io (mobile web emulator).
+
+ATM just a rough implementation.
---
+++ app.js
@@ -0,0 +1,96 @@
+var express = require('express');
+var chokidar = require('chokidar');
+var request = require('request');
+var path = require('path');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var routes = require('./routes/index');
+
+var app = express();
+
+var log = console.log.bind(console);
+
+app.get('/', function(req, res, next) {
+    res.render('index', { url: getIframeUrl() });
+});
+//
+//// Only works on 3000 regardless of what I set environment port to or how I set [value] in app.set('port', [value]).
+app.listen(3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+var pathToArtifact = '/Users/nicola/Git/Dunno-Proximity/app/build/outputs/apk/yolo';
+
+var urlLocation = 'https://storage.evozi.com/apk/dl/15/05/09/com.blacklight.klondike.patience.solitaire.apk?h=EQHgB4jN6eAUi62jJqVDHQ&t=1439094992';
+var token = 'tok_hdzewnreerpguzpyeccu4c4cvr';
+var publicKey = '';
+var privateKey = '';
+var platform = 'android';
+
+//watcher
+
+chokidar.watch('.', {ignored: /[\/\\]\./}).on('all', function (event, path) {
+    console.log(event, path);
+});
+
+var watcher = chokidar.watch('file, dir, or glob', {
+    ignored: /[\/\\]\./,
+    persistent: true
+});
+
+watcher
+    .on('add', function (path) {
+        log('File', path, 'has been added');
+        var options = {
+            uri: 'https://api.appetize.io/v1/app/update',
+            method: 'POST',
+            json: {
+                "token": token,
+                "url": urlLocation,
+                "platform" : platform
+            }
+        };
+        callAppetize(options);
+    })
+    .on('change', function (path) {
+        log('File', path, 'has been changed');
+        var options = {
+            uri: 'https://api.appetize.io/v1/app/update',
+            method: 'POST',
+            json: {
+                "token": token,
+                "url": urlLocation,
+                "platform" : platform,
+                "privateKey": privateKey
+            }
+        };
+        callAppetize(options);
+    })
+
+watcher.on('change', function (path, stats) {
+    if (stats) console.log('File', path, 'changed size to', stats.size);
+});
+
+watcher.add(pathToArtifact);
+
+function callAppetize(options){
+    request(options, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            console.log(body.id)
+            privateKey = body['privateKey'];
+            publicKey = body['publicKey'];
+        }
+    });
+}
+
+function getIframeUrl(){
+    return 'https://appetize.io/embed/'+publicKey+'?device=iphone5s&orientation=portrait&scale=75&xdocMsg=true&deviceColor=black&debug=true&screenOnly=false';
+}
+
+module.exports = app;
